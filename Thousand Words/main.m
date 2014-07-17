a4b09d8c40da548eac9e65c4461e4aa3cd895da7
Fernand LIME <flime@e1r6p12.42.fr>
2014-07-17 12:32:52 +0200

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Fernand LIME on 17/07/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
